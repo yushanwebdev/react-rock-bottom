795aec0d346b90d6f7da13bb7a5239d0d613282b
yushanwebdev <yushanwebdev@gmail.com>
2020-07-23 15:23:52 +0530

    https://reactjs.org/docs/handling-events.html section started to discover.

--- main-concepts/handling-events/src/react-component.js
+++ main-concepts/handling-events/src/react-component.js
@@ -1,17 +1,28 @@
-class LoggingButton extends React.Component {
-  // This syntax ensures `this` is bound within handleClick.  // Warning: this is *experimental* syntax.
-  test = 'yushan'
-  handleClick = () => {    console.log('this is:', this.test);  }
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        Click me
-      </button>
-    );
-  }
+class Toggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+}
+
+handleClick() {
+	this.setState(state => ({
+		isToggleOn: !state.isToggleOn
+	}));
+}
+
+render() {
+	return (
+		<button onClick={this.handleClick}>
+		{this.state.isToggleOn ? 'ON' : 'OFF'}
+		</button>
+		);
+}
 }
 
 ReactDOM.render(
-  <LoggingButton />,
-  document.getElementById('root')
-);
+	<Toggle />,
+	document.getElementById('root')
+	);
